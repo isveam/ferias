414f8c471f3b686fad200166bce307d8335f2579
Ismael e Aurea <velosoismael1@gmail.com>
2019-06-04 22:36:13 -0300

    Criação de layout - projeto

---
+++ app/src/main/java/br/com/etechoracio/calculadora_ferias/SolicitacaoActivity.java
@@ -0,0 +1,31 @@
+package br.com.etechoracio.calculadora_ferias;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitacao);
+    }
+
+    //public void onRadioClick(View v){
+
+      //  if( v.getId()==R.id.radioSim){
+
+            //popular dias
+        // }else{
+            //popular dias
+       // }
+       // }
+       // private ArrayAdapter getAbonoSim(){
+       //     return new ArrayAdapter<Integer>(...,...,Arrays.asList(20,30));
+
+        }
+}
